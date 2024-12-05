ac43c7b9984a9adcd1611d27b3aefce777231277
suxi <2889631270@qq.com>
2024-12-06 02:17:21 +0800

    changes

--- train.sh
+++ train.sh
@@ -1,4 +1,9 @@
-accelerate launch  --multi_gpu --main_process_port 14994 --num_processes 8  --num_machines 1 \
-train.py --image_folder  experiments/SDTL_LOLv1 \
---config ./configs/LOLv1.yml --accelerator_train
+
+
+accelerate launch \
+ --multi_gpu --main_process_port 14994 --num_processes 4  --num_machines 1 \
+train.py --image_folder  experiments/SDTL_LOLv2 \
+--config ./configs/LOLv2_real.yml --accelerator_train 2>&1  | tee "LOLv2_real.log"
+
+
 
